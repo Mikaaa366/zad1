3fc3d405c80aca2653ae128cc249c9e0fc958397
Dominika Janisz <mikaaa366@gmail.com>
2019-11-22 09:01:12 +0100

    instrukcja warunkowa

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+$a=6;
+$b=5;
+
+if ($a>$b) {
+	echo"liczba A jest większa od liczby B i wynosi " .$a;
+}
+else if ($b>$a) {
+	echo"liczba B jest większa od liczby A i wynosi ".$b;
+}
+else if ($a==$b) {
+	echo"obie liczby są takie same" ;
+
+}
+?>
